f5bd3f25a505be59380fd09c4bca278b2059d7fa
Jefry Mammen <145659738+JefryMMN@users.noreply.github.com>
2026-07-05 16:55:34 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -8,14 +8,3 @@ def get_connection():
         password="1234",
         database="complaint_management_ai"
     )
-
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="complaint_system"
-
-    )
